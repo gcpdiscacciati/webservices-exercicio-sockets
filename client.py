b5476e6d5574c6e351d8571a5071c3b44f5cd3de
Giovanne Discacciati <gidiscacciati@gmail.com>
2021-10-13 11:18:54 -0300

    adição de comentários

--- client.py
+++ client.py
@@ -3,15 +3,19 @@
 
 import socket
 
+#IP do servidor, deve ser alterado caso cliente e servidor não estejam na mesma máquina
 serverName = '127.0.0.1'
 serverPort = 16000
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
 
+# Recebe a mensagem do cliente
 message = input("Escreva uma frase: ")
 
+# Envia a mensagem para o servidor
 clientSocket.send(message.encode('utf-8'))
 
+# Recebe e imprime a mensagem processada
 modifiedMessage = clientSocket.recv(1024)
 print(modifiedMessage.decode('utf-8'))
 
--- server.py
+++ server.py
@@ -4,6 +4,7 @@
 import socket
 import re
 
+# Configuração da conexão
 serverPort = 16000
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serverSocket.bind(('', serverPort))
@@ -11,21 +12,26 @@ serverSocket.listen(0)
 
 print("Servidor pronto para receber")
 
+# Checa se a mensagem contém números
 def checkNumbers(mensagem):
     return bool(re.search("\d", mensagem))
 
+# Checa o tamanho médio de caracteres das palavras
 def avgWordLength(lista):
     soma = 0.0
     for word in lista:
         soma += len(word)
     return soma/(len(lista))
 
+# Loop que aguarda a conexão do cliente e processa a mensagem recebida
 while True:
     connectionSocket, addr = serverSocket.accept()
     print("Conexão vinda de {}".format(addr))
     message = connectionSocket.recv(2048)
     print("{} ==> {}".format(addr, message.decode('utf-8')))
     decodedMessage = message.decode('utf-8')
+
+    # Processa a mensagem recebida
     numCaracteres = len(decodedMessage)
     possuiNumeros = checkNumbers(decodedMessage)
     listaPalavras = decodedMessage.split()
@@ -34,8 +40,10 @@ while True:
     modifiedMessage = (f"Número de caracteres = {numCaracteres}"
                        f"\nPossui números = {'Sim' if possuiNumeros else 'Não'}"
                        f"\nQuantidade de palavras = {numPalavras}"
-                       f"\nTamanho médio de caracteres = {tamanhoMedio: .2f}")
+                       f"\nTamanho médio de caracteres das palavras = {tamanhoMedio:.2f}")
 
-    
+    # Retorna a mensagem modificada
     connectionSocket.send(modifiedMessage.encode('utf-8'))
+
+    # Encerra a conexão com o cliente
     connectionSocket.close()
